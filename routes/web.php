eec337e7e17687c671d5cf12ccf7e43330c7db4b
Kevin Gabari <B00137018@mytudublin.ie>
2024-03-11 09:27:59 +0000

    added navbar

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Route::get('products/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
 
+Route::get('products/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
+
 //Route::resource('products', App\Http\Controllers\productController::class);
 
 
